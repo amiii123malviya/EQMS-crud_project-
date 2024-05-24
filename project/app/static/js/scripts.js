ce0eb17105251cf2dcd5239ee1b3e9fd305c1b6c
amisha <amishamalviya2607@gmail.com>
2024-05-24 21:55:53 +0530

    CRUD_completed

--- project/app/static/js/scripts.js
+++ project/app/static/js/scripts.js
@@ -3,3 +3,11 @@ function toggleMenu() {
     navbarLinks.classList.toggle('active');
 }
 console.log("amisha")
+let counter = 1;
+setInterval(function() {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 4) {
+        counter = 1;
+    }
+}, 5000); // Change slide every 5 seconds
